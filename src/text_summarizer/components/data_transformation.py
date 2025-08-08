f5766726f376cb2b9334f31df02e526dcad5ff8a
Prateek <prat6160@gmail.com>
2025-08-08 07:00:16 +0000

    data transformation completed

---
+++ src/text_summarizer/components/data_transformation.py
@@ -0,0 +1,40 @@
+import os
+from text_summarizer.logging import logger
+from transformers import AutoTokenizer
+from datasets import load_dataset, load_from_disk
+from text_summarizer.entity import DataTransformationConfig
+
+
+
+class DataTransformation:
+    def __init__(self, config: DataTransformationConfig):
+        self.config = config
+        self.tokenizer = AutoTokenizer.from_pretrained(config.tokenizer_name)
+
+
+    
+    def convert_examples_to_features(self,example_batch):
+        input_encodings = self.tokenizer(example_batch['article'] , max_length = 1024, truncation = True )
+        
+        with self.tokenizer.as_target_tokenizer():
+            target_encodings = self.tokenizer(example_batch['highlights'], max_length = 128, truncation = True )
+            
+        return {
+            'input_ids' : input_encodings['input_ids'],
+            'attention_mask': input_encodings['attention_mask'],
+            'labels': target_encodings['input_ids']
+        }
+    
+
+    def convert(self):
+        # Load the dataset from CSV files
+        data_path = os.path.join(self.config.data_path, "cnn_dailymail")
+        newspaper_text_summarizer = load_dataset('csv', data_files={
+            'train': os.path.join(data_path, 'train.csv'),
+            'test': os.path.join(data_path, 'test.csv'),
+            'validation': os.path.join(data_path, 'validation.csv')
+        })
+        
+        newspaper_text_summarizer_pt = newspaper_text_summarizer.map(self.convert_examples_to_features, batched = True)
+        newspaper_text_summarizer_pt.save_to_disk(os.path.join(self.config.root_dir,"newspaper_dataset"))
+
